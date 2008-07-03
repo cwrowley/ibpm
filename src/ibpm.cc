0167d91bf97058a74d59b68ff2252b377310649c
Clancy Rowley <cwrowley@princeton.edu>
2008-07-03 04:33:43 +0000

    Added sample main routine.
    git-svn-id: https://svn.princeton.edu/rowley/trunk@16 f381375e-2823-4fb2-bad6-22fa55754e8c

--- src/ibpm.cc
+++ src/ibpm.cc
@@ -1,8 +1,55 @@
+/*! \file ibpm2.cc
+ *  
+ * Sample main routine for IBPM code.
+ */
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
+/*! \brief Setup a timestepper and advance the flow in time
+ *  Just a skeleton of this for now.  Eventually this should read parameters
+ *  from an input file, do checking for bad input, etc.
+ */
 int main(int argc, char* argv[]) {
 	cout << "Hello world!\n";
+	
+	// Setup grid
+	int nx = 100;
+	int ny = 100;
+	double length = 2.0;
+	Grid grid(nx, ny, length);
+	
+	// Setup geometry
+	ifstream infile("geom.inp");
+	assert(infile.good());
+	Geometry geom;
+	geom.load(infile);
+
+	// Setup equations to solve
+	double Reynolds=100;
+	NonlinearNavierStokes model(grid, geom, Reynolds);
+	model.init();
+
+	// Setup timestepper
+	dt = 0.01;
+	RungeKutta3 solver(model, dt);
+
+	// Load initial condition
+	icFile = "initial.bin";
+	State x;
+	x.loadRestartFile(icFile);	
+
+	// Step
+	int numSteps = 100;
+	for(int i=1; i <= numSteps; ++i) {
+		cout << "step " << i << endl;
+		solver.advance(x);		
+
+		// Add something in here to output force data, probe data,
+		// save restart files, etc.
+		// Define a Logger class to handle all of this in one place?
+	}
+		
 	return 0;
 }
