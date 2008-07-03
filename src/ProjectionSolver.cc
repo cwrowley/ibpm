35c40bbb9a9ef8c4505c22aa8c41beb6fddb11da
Clancy Rowley <cwrowley@princeton.edu>
2008-07-03 14:01:37 +0000

    Added ProjectionSolver.cc to repository
    git-svn-id: https://svn.princeton.edu/rowley/trunk@20 f381375e-2823-4fb2-bad6-22fa55754e8c

---
+++ src/ProjectionSolver.cc
@@ -0,0 +1,17 @@
+// ProjectionSolver.cc
+//
+// Description:
+// Documentation about the implementation of the class goes here.
+// No need to duplicate documentation in the header file here.
+// Use standard C++ comments here, as these don't need to go to Doxygen
+//
+// Author(s):
+// Clancy Rowley
+//
+// Date: 3 Jul 2008
+//
+// $Revision: 17 $
+// $LastChangedDate: 2008-07-03 01:55:25 -0400 (Thu, 03 Jul 2008) $
+// $LastChangedBy: clancy $
+// $HeadURL:// $Header$
+
