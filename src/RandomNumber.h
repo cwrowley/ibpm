d3d72e28049c291bf8c2a575395abb76f7a02449
Jonathan Tu <jhtu@princeton.edu>
2009-11-17 23:38:15 +0000

    Added RandomNumber.h to repository.  This class generates a random number, and is taken directly from Numerical Recipes.
    git-svn-id: https://svn.princeton.edu/rowley/ibpm/trunk@327 f381375e-2823-4fb2-bad6-22fa55754e8c

---
+++ src/RandomNumber.h
@@ -0,0 +1,43 @@
+// Random number generator adapted from Numerical Recipes
+#include <iostream>
+
+typedef unsigned long long int Ullong;
+typedef unsigned int Uint;
+typedef double Doub;
+
+using namespace std;
+
+class RandomNumber {
+public:
+    // Constructor. Call with any integer seed (except value of v above).
+    RandomNumber( ) :
+    v(4101842887655102017LL), 
+    w(1),
+    j( time(0) ) {      // Seed with current time (random seed)     
+        u = j ^ v; 
+        getLongInt(); 
+        v = u; 
+        getLongInt(); 
+        w = v; 
+        getLongInt(); 
+    }  
+
+    // Return 64-bit random integer. 
+    inline Ullong getLongInt() {     
+        u = u * 2862933555777941757LL + 7046029254386353087LL; 
+        v ^= v >> 17; v ^= v << 31; v ^= v >> 8; 
+        w = 4294957665U*(w & 0xffffffff) + (w >> 32); 
+        Ullong x = u ^ (u << 21); x ^= x >> 35; x ^= x << 4; 
+        return (x + v) ^ w; 
+    } 
+
+    // Return random double-precision ﬂoating value in the range 0. to 1.
+    inline Doub getDouble() { return 5.42101086242752217E-20 * getLongInt(); } 
+
+    // Return 32-bit random integer.  
+    inline Uint getInt() { return (Uint)getLongInt(); } 
+
+private:
+    Ullong u, v, w;
+    Ullong j;           // initial seed
+};
