7258d8403eca3f5c3546ead08c87f41c6f9ce02e
Steven Brunton <sbrunton@princeton.edu>
2010-05-17 17:18:00 +0000

    Changed MotionFile.h so that motion files specify entire element TSE2 instead of SE2, so that velocity interpolation is cleaner
    git-svn-id: https://svn.princeton.edu/rowley/ibpm/trunk@351 f381375e-2823-4fb2-bad6-22fa55754e8c

--- src/MotionFile.h
+++ src/MotionFile.h
@@ -15,18 +15,24 @@
 
 namespace ibpm {
 
-// Defining a struct for storing (t,x,y,theta), which I call TxSE2
-struct TxSE2 {
-    TxSE2(double t_in, double x_in, double y_in, double theta_in) {
+// Defining a struct for storing (t,x,y,theta), which I call TxTSE2
+struct TxTSE2 {
+    TxTSE2(double t_in, double x_in, double y_in, double theta_in, double dx_in, double dy_in, double dtheta_in) {
         t = t_in;
         x = x_in;
         y = y_in;
         theta = theta_in;
+        dx = dx_in;
+        dy = dy_in;
+        dtheta = dtheta_in;
     }
     double t;
     double x;
     double y;
     double theta;
+    double dx;
+    double dy;   
+    double dtheta;
 };
 
 /*!
@@ -52,14 +58,14 @@ public:
         _filename(filename) {
         ifstream in( filename.c_str() );
         int n;
-        double t,x,y,theta;
+        double t,x,y,theta,dx,dy,dtheta;
         double tlast = -1.e+5;
         in >> n;
         if( in.fail() ) {
             cerr << "ERROR:: MotionFile: file " << filename <<" formatted incorrectly! (err1)" << endl;
         }
         for(int i=0; i<n; i++) {
-            in >> t >> x >> y >> theta;
+            in >> t >> x >> y >> theta >> dx >> dy >> dtheta;
             if( in.fail() ) {
                 cerr << "ERROR:: MotionFile: file " << filename << " formatted incorrectly! (err2)" << endl;
             }
@@ -67,13 +73,13 @@ public:
                 cerr << "ERROR:: MotionFile: time must increase monotonically!" << endl;
             }
             tlast = t;
-            addTxSE2(t,x,y,theta);
+            addTxTSE2(t,x,y,theta,dx,dy,dtheta);
         }   
     }
 
-    /// Adds an element of TxSE2 to the vector
-    void addTxSE2(double t, double x, double y, double theta) {
-        TxSE2 p(t,x,y,theta);
+    /// Adds an element of TxTSE2 to the vector
+    void addTxTSE2(double t, double x, double y, double theta, double dx, double dy, double dtheta) {
+        TxTSE2 p(t,x,y,theta,dx,dy,dtheta);
         // Add the element to the motion file data list
         _data.push_back(p);
     }
@@ -84,27 +90,27 @@ public:
         // find correct index corresponding to current time
         int n=_data.size();
         for(int i=0; i<n-1; i++) {
-            if( (time>_data[i].t) && (time<_data[i+1].t) ) index = i;
-        }     
-        double xout = 0.;
-        double yout = 0.;
-        double thetaout = 0.;
-        double xdotout = 0.;
-        double ydotout = 0.;
-        double thetadotout = 0.;
-        double alpha;
-        double tdiff;
+            if( (time>=_data[i].t) && (time<_data[i+1].t) ) index = i;
+        } 
+        double xo = 0.;
+        double yo = 0.;
+        double thetao = 0.;
+        double dxo = 0.;
+        double dyo = 0.;
+        double dthetao = 0.;
+        double alpha=0.;
+        double tdiff=0.;
         if( index != -1 ) {
             tdiff = _data[index+1].t-_data[index].t;
             alpha = (time-_data[index].t)/tdiff;
-            xout = alpha*_data[index+1].x+(1-alpha)*_data[index].x;
-            yout = alpha*_data[index+1].y+(1-alpha)*_data[index].y;
-            thetaout = alpha*_data[index+1].theta+(1-alpha)*_data[index].theta;
-            xdotout = (_data[index+1].x-_data[index].x)/tdiff;
-            ydotout = (_data[index+1].y-_data[index].y)/tdiff;
-            thetadotout = (_data[index+1].theta-_data[index].theta)/tdiff;
+            xo = alpha*_data[index+1].x+(1-alpha)*_data[index].x;
+            yo = alpha*_data[index+1].y+(1-alpha)*_data[index].y;
+            thetao = alpha*_data[index+1].theta+(1-alpha)*_data[index].theta;
+            dxo = alpha*_data[index+1].dx+(1-alpha)*_data[index].dx;
+            dyo = alpha*_data[index+1].dy+(1-alpha)*_data[index].dy;
+            dthetao = alpha*_data[index+1].dtheta+(1-alpha)*_data[index].dtheta;
         }
-        return TangentSE2( xout, yout, thetaout, xdotout, ydotout, thetadotout );
+        return TangentSE2( xo, yo, thetao, dxo, dyo, dthetao );
     }
 
     inline Motion* clone() const {
@@ -115,8 +121,7 @@ public:
 
 private:
     string _filename;
-    vector<TxSE2> _data;
-    vector<double[4]> _data2;
+    vector<TxTSE2> _data;
 };
 } // namespace ibpm
 
